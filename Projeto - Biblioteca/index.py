c865a418ee4ce3e991296b68705c01e0e52cc800
Micael Dantas <110204915+micaaell@users.noreply.github.com>
2023-12-11 20:27:49 -0300

    Add files via upload

---
+++ Projeto - Biblioteca/index.py
@@ -0,0 +1,55 @@
+from templates.manterlivroUI import ManterLivroUI
+from templates.manterexemplarUI import ManterExemplarUI
+from templates.mantergeneroUI import ManterGeneroUI
+from templates.loginUI import LoginUI
+from templates.listarlivrosUI import ListarLivroUI
+from templates.editarperfilUI import EditarPerfilUI
+from views import View
+
+import streamlit as st
+
+
+
+class IndexUI:
+    def menu_visitante():
+      op = st.sidebar.selectbox("Menu", ["Login","Todos os livros"])
+      if op == "Login": LoginUI.main()
+      if op == "Todos os livros": ListarLivroUI.main()
+      
+    def menu_admin():
+      op = st.sidebar.selectbox("Menu", ["Livros", "Exemplares", "Gêneros", "Editar Perfil"])
+      if op == "Livros": ManterLivroUI.main()
+      if op == "Exemplares": ManterExemplarUI.main()
+      if op == "Gêneros": ManterGeneroUI.main()
+      if op == "Editar Perfil": EditarPerfilUI.main()
+      
+    def menu_cliente():
+      op = st.sidebar.selectbox("Menu", ["Todos os livros"])
+      if op == "Todos os livros": ListarLivroUI.main()
+
+    def btn_logout():
+      if st.sidebar.button("Logout"):
+        del st.session_state["cliente_id"]
+        del st.session_state["cliente_nome"]
+      
+    def sidebar():
+      if "cliente_id" not in st.session_state:
+        IndexUI.menu_visitante()   
+      else:
+        st.sidebar.write("Bem-vindo(a), " + st.session_state["cliente_nome"])
+        if st.session_state["cliente_nome"] == "admin": IndexUI.menu_admin()
+        else:IndexUI.menu_cliente()
+        IndexUI.btn_logout() 
+          
+
+    def main():
+      View.cliente_admin()
+      IndexUI.sidebar()
+
+      #if "page" not in st.session_state: st.session_state["page"] = "equacaoUI"
+      #if st.session_state["page"] == "manter_clienteUI": ManterClienteUI.main()
+
+IndexUI.main()
+
+
+
---
+++ Projeto - Biblioteca/views.py
@@ -0,0 +1,75 @@
+from models.livro import Livro, NLivros
+from models.exemplar import Exemplar, NExemplar
+from models.genero import Genero, NGenero
+from models.cliente import Cliente, NCliente
+import datetime
+
+class View:
+  def livro_inserir(nome,idGenero, datapu, autor,editora):
+    livro = Livro(0, nome, idGenero,datapu, autor,editora)
+    NLivros.inserir(livro)
+
+  def livro_listar():
+    return NLivros.listar()
+  
+  def livro_listar_nomes():
+    return NLivros.listar_nomes()
+  
+  def livro_listar_id(id):
+    return NLivros.listar_id(id)
+
+  def livro_atualizar(id, nome, idGenero,datapu, autor,editora):
+    livro = Livro(id, nome,idGenero, datapu, autor,editora)
+    NLivros.atualizar(livro)
+    
+  def livro_excluir(id):
+    livro = Livro(id, "", "", "","","")
+    NLivros.excluir(livro)
+  
+  def cliente_admin():
+    for cliente in View.cliente_listar():
+      if cliente.get_nome() == "admin": return
+    View.cliente_inserir("admin", "admin", "0000", "admin")  
+
+  
+  def cliente_login(email, senha):
+    for cliente in View.cliente_listar():
+      if cliente.get_email() == email and cliente.get_senha() == senha:
+        return True
+    return False
+
+  def cliente_login(email, senha):
+    for cliente in View.cliente_listar():
+      if cliente.get_email() == email and cliente.get_senha() == senha:
+        return cliente
+    return None  
+  
+  def cliente_listar():
+    return NCliente.listar()
+
+  def exemplar_listar():
+    return NExemplar.listar()
+
+  def exemplar_listar_id(id):
+    return NExemplar.listar_id(id)
+
+  def exemplar_inserir(idLivro, unidade, local):
+    NExemplar.inserir(Exemplar(0, idLivro, unidade, local))
+
+  def exemplar_atualizar(id, idLivro, unidade, local):
+    NExemplar.atualizar(Exemplar(id, idLivro, unidade, local))
+
+  def exemplar_excluir(id):
+    NExemplar.excluir(Exemplar(id, "", "", ""))
+
+  def genero_listar():
+    return NGenero.listar()
+
+  def genero_inserir(genero):
+    NGenero.inserir(Genero(0, genero))
+
+  def genero_atualizar(id,genero):
+    NGenero.atualizar(Genero(id, genero))
+
+  def genero_excluir(id):
+    NGenero.excluir(Genero(id, ""))
