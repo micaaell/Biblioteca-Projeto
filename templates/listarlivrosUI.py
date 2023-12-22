08ccc79a420b090dd880a844712ff8ee77a532ea
Micael Dantas <110204915+micaaell@users.noreply.github.com>
2023-12-22 10:18:40 -0300

    Add files via upload

---
+++ templates/listarlivrosUI.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import pandas as pd
+from views import View
+import time
+
+class ListarLivroUI:
+  def main():
+    st.header("Livros Cadastrados")
+    tab_names = ["Listar","Listar por gênero"]
+    selected_tab = st.radio("Escolha uma das opções:", tab_names)
+    if selected_tab == "Listar":
+        ListarLivroUI.listar()
+    if selected_tab == "Listar por gênero":
+        ListarLivroUI.listarporgenero()
+
+  def listar():
+    livros = View.livro_listar()
+    if len(livros) == 0:
+      st.write("Nenhum livro cadastrado")
+    else:
+      dic = []
+      for obj in livros: dic.append(obj.__dict__)
+      df = pd.DataFrame(dic)
+      st.dataframe(df)
+
+
+  def listarporgenero():
+    livros = View.livro_listar()
+    generos = View.genero_listar()
+    idGenero = st.selectbox("Selecione o Gênero", generos)
+
+    
+    if len(livros) == 0:
+      st.write("Nenhum livro cadastrado")
+    else:
+      dic = []
+      for obj in livros:
+        dic.append(obj.__dict__)
+
+    if len(generos) == 0:
+      st.write("Nenhum genero cadastrado")
+    else:
+      dic = []
+      for obj in View.livro_listar_genero(idGenero.get_genero()):
+        dic.append(obj.__dict__)
+      df = pd.DataFrame(dic)
+      st.dataframe(df)
+
+
+
+
+      
---
+++ views.py
@@ -0,0 +1,90 @@
+from models.livro import Livro, NLivros
+from models.exemplar import Exemplar, NExemplar
+from models.genero import Genero, NGenero
+from models.cliente import Cliente, NCliente
+import datetime
+
+class View:
+  def livro_inserir(idGenero,nome, datapu, autor,editora):
+    livro = Livro(0,idGenero,nome,datapu, autor,editora)
+    NLivros.inserir(livro)
+
+  def livro_listar():
+    return NLivros.listar()
+
+  def livro_listar_genero(idGenero):
+    genero=[]
+    for livro in View.livro_listar():
+      if livro.get_idGenero() == idGenero:
+        genero.append(livro)
+    return genero
+  
+  def livro_listar_nomes():
+    return NLivros.listar_nomes()
+  
+  def livro_listar_id(id):
+    return NLivros.listar_id(id)
+
+  def livro_atualizar(id, idGenero,nome,datapu, autor,editora):
+    livro = Livro(id,idGenero,nome, datapu, autor,editora)
+    NLivros.atualizar(livro)
+    
+  def livro_excluir(id):
+    livro = Livro(id, "", "", "","","")
+    NLivros.excluir(livro)
+
+  def cliente_inserir(nome, email, fone, senha):
+    cliente = Cliente(0, nome, email, fone, senha)
+    NCliente.inserir(cliente)
+  
+  def cliente_admin():
+    for cliente in View.cliente_listar():
+      if cliente.get_nome() == "admin": return
+    View.cliente_inserir("admin", "admin", "0000", "admin")  
+
+  
+  def cliente_login(email, senha):
+    for cliente in View.cliente_listar():
+      if cliente.get_email() == email and cliente.get_senha() == senha:
+        return True
+    return False
+
+  def cliente_login(email, senha):
+    for cliente in View.cliente_listar():
+      if cliente.get_email() == email and cliente.get_senha() == senha:
+        return cliente
+    return None  
+  
+  def cliente_listar():
+    return NCliente.listar()
+  
+  def cliente_atualizar(id, nome, email, fone, senha):
+    cliente = Cliente(id, nome, email, fone, senha)
+    NCliente.atualizar(cliente)
+
+  def exemplar_listar():
+    return NExemplar.listar()
+
+  def exemplar_listar_id(id):
+    return NExemplar.listar_id(id)
+
+  def exemplar_inserir(idLivro, unidade, local):
+    NExemplar.inserir(Exemplar(0, idLivro, unidade, local))
+
+  def exemplar_atualizar(id, idLivro, unidade, local):
+    NExemplar.atualizar(Exemplar(id, idLivro, unidade, local))
+
+  def exemplar_excluir(id):
+    NExemplar.excluir(Exemplar(id, "", "", ""))
+
+  def genero_listar():
+    return NGenero.listar()
+
+  def genero_inserir(genero):
+    NGenero.inserir(Genero(0, genero))
+
+  def genero_atualizar(id,genero):
+    NGenero.atualizar(Genero(id, genero))
+
+  def genero_excluir(id):
+    NGenero.excluir(Genero(id, ""))
